90e5467c2cef047efbc1405ac8042a182b35a215
kevin_xiang <xiangjie55@qq.com>
2017-03-15 19:03:42 +0800

    更新

---
+++ src/main/java/com/example/junit/DemoBeanTest.java
@@ -0,0 +1,26 @@
+package com.example.junit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.config.JavaConfig;
+import com.example.model.DemoBean;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={JavaConfig.class})
+@ActiveProfiles("prod")
+public class DemoBeanTest {
+	
+	@Autowired
+	private DemoBean demoBean;
+	
+	@Test
+	public void demoBeanProfilesTest(){
+		System.out.println( "junit : " + demoBean.getContent() );
+	}
+	
+}
